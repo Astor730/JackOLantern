b08dbaf978678b5522b2f29008270067f7936c11
BT_1N3_26 <mister.p730@gmail.com>
2018-11-26 14:39:45 -0500

    Constructor

---
+++ src/JackOLantern.java
@@ -0,0 +1,9 @@
+public class JackOLantern extends Object
+{
+    private String[][] faceFeatures;
+
+    public JackOLantern(String[][] faceFeatures)
+    {
+        this.faceFeatures = faceFeatures;
+    }
+}
