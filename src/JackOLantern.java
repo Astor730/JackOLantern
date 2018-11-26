832dd01e446a73f4785ee21add08ae501ff850ca
BT_1N3_26 <mister.p730@gmail.com>
2018-11-26 14:46:48 -0500

    Methods

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -1,9 +1,21 @@
-public class JackOLantern extends Object
+public class JackOLantern
 {
     private String[][] faceFeatures;
 
     public JackOLantern(String[][] faceFeatures)
     {
         this.faceFeatures = faceFeatures;
+    }
+    public void edit(String replace, int row, int column)
+    {
+        replace = faceFeatures[row][column];
+    }
+    public void fill(String str)
+    {
+
+    }
+    public String toString()
+    {
+        return "hello";
     }
 }
