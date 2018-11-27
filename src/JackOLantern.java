543886298a44c27bc8ec741502ae51225098a765
BT_1N3_26 <mister.p730@gmail.com>
2018-11-27 14:34:33 -0500

    Done

--- src/JackOLantern.java
+++ src/JackOLantern.java
@@ -3,19 +3,35 @@ public class JackOLantern
     private String[][] faceFeatures;
 
     public JackOLantern(String[][] faceFeatures)
+
     {
         this.faceFeatures = faceFeatures;
     }
     public void edit(String replace, int row, int column)
     {
-        replace = faceFeatures[row][column];
+        faceFeatures[row][column] = replace;
     }
     public void fill(String str)
     {
-
+        for (int i = 0; i<faceFeatures.length;i++)
+        {
+            for (int x = 0; x<faceFeatures[i].length;x++)
+            {
+                faceFeatures[i][x] = str;
+            }
+        }
     }
     public String toString()
     {
-        return "hello";
+        String output = "";
+        for(int i =0; i<faceFeatures.length;i++)
+        {
+            for(int x = 0;x<faceFeatures[i].length ; x++)
+            {
+                output = output + faceFeatures[i][x];
+            }
+            output = output + "\n";
+        }
+        return output;
     }
 }
