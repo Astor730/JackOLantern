190eac00fc16be1f135a96715ae7b35643fa35e2
BT_1N3_26 <mister.p730@gmail.com>
2018-11-27 14:40:39 -0500

    Done2

--- src/Runner.java
+++ src/Runner.java
@@ -6,10 +6,10 @@ public class Runner
         JackOLantern face1 = new JackOLantern(face);
 
         face1.fill("x");
-        face1.edit("",0,0);
-        face1.edit("",4,7);
-        face1.edit("",0,7);
-        face1.edit("",4,0);
+        face1.edit(" ",0,0);
+        face1.edit(" ",4,7);
+        face1.edit(" ",0,7);
+        face1.edit(" ",4,0);
 
         face1.edit("0",1,2);
         face1.edit("0",1,5);
